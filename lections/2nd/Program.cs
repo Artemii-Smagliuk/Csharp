fcb4b617b90976fb54806d874576ba4822beae8e
smaglart <smaglart@mail.ru>
2022-12-07 20:06:12 +0300

    Finished the 2nd lection

--- lections/2nd/Program.cs
+++ lections/2nd/Program.cs
@@ -111,6 +111,7 @@ while (index < n)
 
 */
 //....................Ещё с массивами (создали массив, заполнили рандомными числами и распечатали)
+/*
 void FillArray(int[]collection)
 {
     int length = collection.Length;
@@ -135,4 +136,58 @@ int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
 
-//Ещё нужно разобрать применение вышенаписанного к задаче
+..........................Теперь применение вышеописанного к задача
+*/
+void FillArray(int[]collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);      
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index ++;
+    }
+    return position;
+}
+
+
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array,444);
+Console.WriteLine(pos);
