46eecc00882af7c7e69ec15832a31d96be5ebf17
smaglart <smaglart@mail.ru>
2022-12-23 13:59:05 +0300

    Finished 6th hw

---
+++ seminars/6th/hw/task41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void PosNum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) { result++; }
+    }
+    Console.Write($"[{string.Join(",", array)}] Чисел больше нуля: ");
+    Console.Write(result);
+}
+
+int[] EnterData(int[] array)
+{
+    int[] result = { };
+    for (int i = 0; i + 1 > 0; i++)
+    {
+        Console.Write("Введите число, для завершения дважды нажмите Enter : ");
+        string str = Console.ReadLine();
+        int var = 0;
+        double number;
+        bool isNumber = double.TryParse(str, out number);
+        if (str != "")
+        {
+            if (isNumber == true)
+            {
+                Array.Resize(ref array, i + 1);
+                var = Int32.Parse(str);
+                array[i] = var;
+            }
+        }
+        if (str == "") { break; }
+    }
+    return array;
+}
+
+int[] arr = { };
+arr = EnterData(arr);
+PosNum(arr);
---
+++ seminars/6th/hw/task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Intersec(double k1, double b1, double k2, double b2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * x) + b2;
+    Console.WriteLine($"[b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2}] Точка пересечения этих двух прямых: ({x}; {y})");
+}
+
+double EnterData(string text)
+{
+    Console.Write(text);
+    double var = double.Parse(Console.ReadLine());
+    return var;
+}
+
+double b1 = EnterData("Задайте значение b1: ");
+double k1 = EnterData("Задайте значение k1: ");
+double b2 = EnterData("Задайте значение b2: ");
+double k2 = EnterData("Задайте значение k2: ");
+Intersec(k1, b1, k2, b2);
